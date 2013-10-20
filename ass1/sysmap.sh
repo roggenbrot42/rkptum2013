b4ed0e5e6445db11339f10b24e9c18c5fe4d5873
wenwen <wenwen@debian>
2013-10-20 21:21:38 +0200

    comments

--- ass1/sysmap.sh
+++ ass1/sysmap.sh
@@ -3,7 +3,9 @@
 FILEPATH="/boot/System.map-$(uname -r)"
 # declare output header file name
 HFILE="sysmap.h"
-# create a header file with preprocessor
+# create preprocessor statement
 echo "$HFILE" | awk '{ gsub("\.","_"); print "#ifndef "toupper($0)"\n#define "toupper($0) }' > $HFILE
+# parse right symbols to the header file
 awk '/^(.*) (D|R|T) (.*)$/ { print "#define "$3"_"$2" 0x"$1 }' $FILEPATH >> $HFILE 
+# end
 echo "#endif" >> $HFILE
