394121396c41be66e9698ab23028130584bae803
root <root@debian>
2013-10-21 18:56:11 +0200

    now works

--- ass1/mod.c
+++ ass1/mod.c
@@ -9,21 +9,38 @@
 #endif
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/kernel.h> /* Needed for KERN_INFO */
-#include <linux/sched.h>
-void print_nr_procs()
+#include <linux/init.h> /* Needed for the macros, hints for linking and loading, see http://tldp.org/LDP/lkmpg/2.6/html/x245.html */
+// Why can nr_processes be called directly?
+//#include <linux/sched.h>
+#include "sysmap.h"
+
+#define DRIVER_AUTHOR "Rootkit Programming"
+#define DRIVER_DESC   "Assigment 1 - 5 LKM Programming"
+static void print_nr_procs()
 {
-  printk(KERN_INFO "Number of current running processes: %d", nr_processes());
+  // cast system symbol adress to function pointer
+  int a = ((int (*)(void))nr_processes_T)();
+  printk(KERN_INFO "Number of current running processes: %d\n", a);
 }
 
-int init_module(void)
+static int __init mod_init(void)
 {
   printk(KERN_INFO "Welcome!\n");
   print_nr_procs();
+
   return 0;
 }
 
-void cleanup_module(void)
+static void __exit mod_exit(void)
 {
   printk(KERN_INFO "Goodbye!\n");
 }
 
+
+    
+module_init(mod_init);
+module_exit(mod_exit);
+
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_LICENSE("GPL");
